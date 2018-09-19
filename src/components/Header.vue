a9d4dd5c4cfb92369fe457b6dcf7751add2826e0
jonathan <>
2018-09-19 15:21:16 -0400

    menu and slider working

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,57 @@
 <template>
-    <header class="container">
-        <img class="header__logo" alt="Vue logo" src="@/assets/img/your-logo.png">
+    <div>
+        <header>
+            <img class="header__menu" alt="Vue logo" onclick="openNav()" src="@/assets/img/hamburger.png">
+            
+            <img class="header__logo" alt="Vue logo" src="@/assets/img/hab-la_logo.png">
+            <img class="header__menu" alt="Vue logo" src="@/assets/img/search.png">
+            <!-- <h1 class="header__logo">hab-la</h1> -->
 
-    </header>
+
+            <!-- The overlay -->
+        
+        </header>
+
+            <div id="myNav" class="overlay-menu">
+            
+              <!-- Button to close the overlay navigation -->
+              <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            
+              <!-- Overlay content -->
+              <div class="container overlay-content">
+                <div class="container left-menu">
+                    <ul class="left-menu__top">
+                        <a href="#">HOME</a>
+                        <a href="#">EVENTS</a>
+                        <a href="#">ARTICLES</a>
+                    </ul>
+                    <div class="partners">
+                        <h1>PARTNERS</h1>
+                        <div >
+                            <img class="partners__logos" src="@/assets/img/partners.png" alt="">
+                            <img src="" alt="">
+                            <img src="" alt="">
+                            <img src="" alt="">
+                            <img src="" alt="">
+                            <img src="" alt="">
+                        </div>
+                    </div>
+                    <div class="contact-menu">
+                        <h1>CONTACT</h1>
+                        <span>CAMILO ESPITIA / 305.123.1234 / CAMILO@HABLA.COM</span>
+                    </div>
+                </div>
+                <div class="about">
+                    <h1>ABOUT HAB-LA</h1>
+                    <p>smart cities : planning Urban planning has direct consequences on the population. It can promote equality as much as it can increase the social gap between different sectors with catastrophic consequences for all its citizens. Furthermore, the survival of these cities depends on the decisions made now. The move from rural settings into the cities will only keep growing and cities will need to have the flexibility to accommodate for this growth. Technologies will need to be implemented to make the necessary shifts in how resources are used for the demand increase of housing, employment and education. Governments will need to look at how their economy is handled both locally and globally and its impact on the population to make sure affected economic sectors will have room for adjustment. The agricultural sector, for example, sees itself heavily wedged by international trade agreements in which local products lose value, causing their producers (in this case farmers) to move elsewhere for economic opportunities.</p>
+                    <img src="@/assets/img/hab-la_logo.png" alt="">
+                </div>
+              </div>
+            </div>
+    </div>
 </template>
+
+
+
+
+	
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,49 @@
 <template>
   <div>
       <Header/>
+      
         <div id="carouselExampleIndicators" class="carousel slide custom-slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img class="d-block w-100" src="https://placeimg.com/1000/980/tech/grayscale" alt="First slide">
+                <img class="d-block h-100 w-100" src="@/assets/img/slides/slide0.jpg" alt="First slide">
+                    <div class="overlay"></div>
                     <div class="carousel-caption d-none d-md-block">
-                        <div class="carousel-caption__slide-num">02_</div>
-                        <div class="carousel-caption__slide-heading">OUMEZINE</div>
-                        <div class="carousel-caption__slide-sub-heading">MARCH 2018</div>
+                        <div class="carousel-caption__slide-title">HABITAT</div>
+                        <div class="carousel-caption__slide-heading">LATIN AMERICA</div>
                     </div>
                 </div>
                 <div class="carousel-item">
-                <img class="d-block w-100" src="https://placeimg.com/1000/980/arch/grayscale" alt="Second slide">
-                <div class="carousel-caption d-none d-md-block">
-                        <div class="carousel-caption__slide-num">03_</div>
-                        <div class="carousel-caption__slide-heading">OUMEZINE</div>
-                        <div class="carousel-caption__slide-sub-heading">MARCH 2018</div>
+                <img class="d-block h-100 w-100" src="@/assets/img/slides/slide1.jpg" alt="Second slide">
+                    <div class="overlay"></div>
+                    <div class="carousel-caption d-none d-md-block">
+                        <div class="carousel-caption__slide-title">LIMA</div>
+                        <div class="carousel-caption__slide-heading">PERU</div>
                     </div>
                 </div>
-                <!-- <div class="carousel-item">
-                <img class="d-block w-100" src="..." alt="Third slide">
-                </div> -->
+                <div class="carousel-item">
+                <img class="d-block h-100 w-100" src="@/assets/img/slides/slide2.jpg" alt="third slide">
+                    <div class="overlay"></div>
+                    <div class="carousel-caption d-none d-md-block">
+                        <div class="carousel-caption__slide-title">MEDELLIN</div>
+                        <div class="carousel-caption__slide-heading">COLOMBIA</div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                <img class="d-block h-100 w-100" src="@/assets/img/slides/slide3.jpg" alt="fourth slide">
+                    <div class="overlay"></div>
+                    <div class="carousel-caption d-none d-md-block">
+                        <div class="carousel-caption__slide-title">MANAGUA</div>
+                        <div class="carousel-caption__slide-heading">NICARAGUA</div>
+                    </div>
+                </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <a class="carousel-control-prev arrows" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
@@ -38,9 +53,12 @@
             </a>
         </div>
 
-        <section class="container intro">
-            <img src="@/assets/img/intro-shoe.jpg" alt="">
-            <img class="intro-logo" src="@/assets/img/your-logo.png" alt="">
+        <section class="container intro-section">
+            <span class="intro-section__title">WHAT WE ARE</span>
+            <div class="intro-section__text">
+                <h1>A BETTER UNDERSTANDING OF</h1>
+                <span>LOCAL PLANNING</span>
+            </div>
         </section>
         <section class="divider2">
             <button class="btn outline-success divider2-btn">BUTTON</button>
@@ -61,7 +79,7 @@
             </div>
             <button class="btn vv-button">SHOP MORE</button>
         </section>
-
+        
         <section class="men-shoes">
             <div class="container men-shoes-content">
                 <img class="" src="@/assets/img/your-logo.png" alt="">
